4634982845a2a1f74b8fd0dfac9cd181d534bcfb
Bonnie Lam <103082439+lambonnie7@users.noreply.github.com>
2023-04-26 15:55:51 -0700

    Add function to find the nth value

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(nth) {
+    if (nth < 0) return 'OOPS';
+    let secondPrecedingNumber = 0;
+    let precedingNumber = 1;  
+    for (let i = 1; i < nth; i++) {
+        const newNumber = secondPrecedingNumber + precedingNumber;
+        secondPrecedingNumber = precedingNumber;
+        precedingNumber = newNumber;
+    }
+    return precedingNumber;
 };
 
 // Do not edit below this line
