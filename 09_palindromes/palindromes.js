7278bc547b68773c4447b57381e282fb8236b147
Bonnie Lam <103082439+lambonnie7@users.noreply.github.com>
2023-04-25 19:32:02 -0700

    Add function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    const noSpecialCharactersWord = word.replace(/[^a-zA-Z0-9]/g, '');
+    const backwardsWord = noSpecialCharactersWord.split('').reverse().join('');
+    if (noSpecialCharactersWord.toLowerCase() == backwardsWord.toLowerCase()) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
